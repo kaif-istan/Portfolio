9a06c3bf79696202aa1730055642a56c0b77d88e
Md Kaif <kaif85077@gmail.com>
2025-09-25 23:49:37 +0530

    newslater not used fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // import Blogs from "./components/Blogs";
 import Hero from "./components/Hero";
-import Newsletter from "./components/Newsletter";
+// import Newsletter from "./components/Newsletter";
 import Projects from "./components/Projects";
 
 
